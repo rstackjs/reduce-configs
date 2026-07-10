e5a2bd4f5e32f6fbefd345943bf3bd4816380b56
neverland <chenjiahan.jait@bytedance.com>
2026-07-10 10:54:15 +0800

    chore: migrate to rstack cli

---
+++ rstack.config.ts
@@ -0,0 +1,19 @@
+import { define } from 'rstack';
+
+define.lib({
+  lib: [{ syntax: 'es2023', dts: true }],
+});
+
+define.lint(async () => {
+  const { js, ts } = await import('rstack/lint');
+
+  return [
+    js.configs.recommended,
+    ts.configs.recommended,
+    {
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
+  ];
+});
